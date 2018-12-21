b9a41ad3da4046d785e2f48fc7f6a8c0f48ef81a
sunyonghua <sunyh@mjoys.com>
2018-12-21 14:39:43 +0800

    类型

--- ts-demo01.ts
+++ ts-demo01.ts
@@ -1,4 +1,26 @@
-let a: number = 10000;
-let b: string = 'is string';
-let c: boolean = true;
-let d: any = 1; // any 任何类型
+// 定义数值
+const a: number = 10000;
+const floatA: number = 100.1;
+
+// 定义string
+const b: string = 'is string';
+// const ObjB:string = new String('nihao') error 因为new String 是包装类生成的其实是个Object
+// const ObjB:Object = new String('1212')    success 编译通过
+
+// 定义布尔值
+const c: boolean = true;
+// const BoolC:boolean = new Boolean(1)  道理同 new String() ↑
+
+// 定义undefined
+const u: undefined = undefined;
+
+// 定义null
+const n: null = null;
+
+// any 任何类型
+let d: any = 1;
+d = '1';
+// 变量未声明未赋值那么也是any类型
+let anyD;
+anyD = '1';
+anyD = 1;
